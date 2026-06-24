59f742f981e1e78ae13f272ad504ed6015493808
ODEF <103530614+ODEF@users.noreply.github.com>
2026-06-25 02:21:05 +0400

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Healthy Paw</h1>
+      <p>Website is working on Vercel.</p>
+    </main>
+  );
+}
